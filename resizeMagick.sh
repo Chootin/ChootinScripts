db23063ce4980d4814f99b4fb3befb61d2af110c
Chootin <chootin.apps@gmail.com>
2017-01-20 17:07:40 +1000

    Updating resizemagick to check number of arguments

--- resizeMagick.sh
+++ resizeMagick.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 # Uses ImageMagick
 
+if [ "$#" -ne 2 ]; then
+    echo "Usage: resizeMagick <Input file> <Scale percentage>"
+    exit
+fi
 filename=$(basename "$1")
 extension="${filename##*.}"
 filenameNoExtension="${filename%.*}"
