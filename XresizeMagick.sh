6d26c2aa0611810188a97bd043a5751132951eb0
Chootin <chootin.apps@gmail.com>
2017-01-20 19:14:26 +1000

    Added support for multiple file operations

--- XresizeMagick.sh
+++ XresizeMagick.sh
@@ -1,17 +1,19 @@
 #! /bin/bash
 # Uses ImageMagick, Xdialog
 
-if [ "$#" -ne 1 ]; then
+if [ "$#" -eq 0 ]; then
     echo "Usage: resizeMagick <Input file>"
     exit
 fi
 
-filename=$(basename "$1")
-extension="${filename##*.}"
-filenameNoExtension="${filename%.*}"
-
 Xdialog --inputbox "Input Percentage" 10 40 2>/tmp/resizemagick.perc.tmp
 percentage=`cat /tmp/resizemagick.perc.tmp`
-percentage="${percentage%}"
+percentage="${percentage//'%'}"
 
-convert "$filename" -resize "$percentage"% "$filenameNoExtension"_resized."$extension"
+for var in "$@" do
+    filename=$(basename "$var")
+    extension="${filename##*.}"
+    filenameNoExtension="${filename%.*}"
+
+    convert "$filename" -resize "$percentage"% "$filenameNoExtension"_resized."$extension"
+done
